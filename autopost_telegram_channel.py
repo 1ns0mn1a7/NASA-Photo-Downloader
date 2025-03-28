dd6d93eda252b3734a1f50d7921743b09d61f062
1ns0mn1a7 <tyrischev.d.v@gmail.com>
2025-03-28 04:52:20 +0300

    update scripts

--- autopost_telegram_channel.py
+++ autopost_telegram_channel.py
@@ -4,58 +4,52 @@ import random
 import argparse
 from dotenv import load_dotenv
 import telegram
+from telegram_tools import get_images, send_photo_to_channel
 
 
-load_dotenv()
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
-DEFAULT_INTERVAL = int(os.getenv("PUBLISH_INTERVAL", 4 * 60 * 60))
-
-bot = telegram.Bot(token=TOKEN)
-
-
-def send_photo_to_channel(photo_path: str):
-    with open(photo_path, "rb") as photo:
-        bot.send_photo(chat_id=CHANNEL_ID, photo=photo)
-
-
-def get_images_list(directory: str):
-    return [os.path.join(directory, filename)
-            for filename in os.listdir(directory)
-            if filename.lower().endswith((".png", ".jpg", ".jpeg"))]
-
-
-def publish_photos(directory: str, interval: int):
+def publish_photos(bot, directory: str, interval: int, channel_id: str):
     while True:
-        images = get_images_list(directory)
+        images = get_images(directory)
         if not images:
             time.sleep(interval)
             continue
         random.shuffle(images)
         for image in images:
-            send_photo_to_channel(image)
+            send_photo_to_channel(bot, image, channel_id)
             time.sleep(interval)
 
 
 def main():
+    load_dotenv()
+    telegram_bot_token = os.getenv("TELEGRAM_TOKEN")
+    telegram_channel_id = os.getenv("TELEGRAM_CHANNEL_ID")
+    default_interval = int(os.getenv("PUBLISH_INTERVAL", 4 * 60 * 60))
+
+    bot = telegram.Bot(token=telegram_bot_token)
+
     parser = argparse.ArgumentParser(
         description="Скрипт для публикации фотографий в Telegram-канал."
-        )
+    )
     parser.add_argument(
         "--directory",
         type=str,
         default="images",
         help="Папка с изображениями (по умолчанию 'images')."
-        )
+    )
     parser.add_argument(
         "--interval",
         type=int,
-        default=DEFAULT_INTERVAL,
+        default=default_interval,
         help="Интервал публикации в секундах (по умолчанию 4 часа)."
-        )
+    )
     arguments = parser.parse_args()
-    publish_photos(arguments.directory, arguments.interval)
+
+    publish_photos(
+        bot,
+        arguments.directory,
+        arguments.interval,
+        telegram_channel_id
+    )
 
 
 if __name__ == "__main__":
--- post_telegram_channel.py
+++ post_telegram_channel.py
@@ -3,58 +3,57 @@ import random
 import argparse
 from dotenv import load_dotenv
 import telegram
+from telegram_tools import get_images, send_photo_to_channel
 
 
-load_dotenv()
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
-
-bot = telegram.Bot(token=TOKEN)
-
-
-def send_photo_to_channel(photo_path: str):
-    with open(photo_path, "rb") as photo:
-        bot.send_photo(chat_id=CHANNEL_ID, photo=photo)
-
-
-def get_images_list(directory: str):
-    return [os.path.join(directory, filename)
-            for filename in os.listdir(directory)
-            if filename.lower().endswith((".png", ".jpg", ".jpeg"))]
-
-
-def publish_photo(directory: str, choose_photo: str = None):
-    images = get_images_list(directory)
+def publish_photo(
+        bot,
+        directory: str,
+        channel_id: str,
+        choose_photo: str = None
+):
+    images = get_images(directory)
 
     if choose_photo:
         choose_photo_path = os.path.join(directory, choose_photo)
         if choose_photo_path in images:
-            send_photo_to_channel(choose_photo_path)
+            send_photo_to_channel(bot, choose_photo_path, channel_id)
     else:
         if images:
             random_image = random.choice(images)
-            send_photo_to_channel(random_image)
+            send_photo_to_channel(bot, random_image, channel_id)
 
 
 def main():
+    load_dotenv()
+    telegram_bot_token = os.getenv("TELEGRAM_TOKEN")
+    telegram_channel_id = os.getenv("TELEGRAM_CHANNEL_ID")
+
+    bot = telegram.Bot(token=telegram_bot_token)
+
     parser = argparse.ArgumentParser(
         description="Скрипт для публикации фотографии в Telegram-канал."
-        )
+    )
     parser.add_argument(
         "--directory",
         type=str,
         default="images",
         help="Папка с изображениями (по умолчанию 'images')."
-        )
+    )
     parser.add_argument(
         "--photo",
         type=str,
         help="Название фотографии для публикации "
              "(если не указано, публикуется случайная)."
-        )
+    )
     arguments = parser.parse_args()
-    publish_photo(arguments.directory, arguments.photo)
+
+    publish_photo(
+        bot,
+        arguments.directory,
+        telegram_channel_id,
+        arguments.photo
+    )
 
 
 if __name__ == "__main__":
